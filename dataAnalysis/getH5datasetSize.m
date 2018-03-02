0944c1915324b760605780d84585bc43b6bdc96d
Jewel Y Chu <jewel.y.chu@gmail.com>
2018-03-02 08:06:50 -0800

    small fixes

--- dataAnalysis/getH5datasetSize.m
+++ dataAnalysis/getH5datasetSize.m
@@ -6,10 +6,11 @@
 %           dataset - name of the dataset of interest
 %
 %   Output: size - dataset size (column size for matrix)
+%
 %                                   by Jewel Y. Lee (last updated: 2/27/18)
 function size = getH5datasetSize(h5file, dataset)
 %h5file = 'tR_1.0--fE_0.90_10000';
-dataset = 'spikesProbedNeurons';
+%dataset = 'spikesProbedNeurons';
 
 outfile = [dataset '.txt'];
 if exist(outfile, 'file')==2

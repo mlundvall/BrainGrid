e4fe2fa9369979a2677840026f1c3bc50e97e99f
Jewel Y Chu <jewel.y.chu@gmail.com>
2018-04-03 02:38:13 -0700

    files for generating sequences with different window and gap size (for ML analysis)

--- matlab/burst-analysis/makeSequence.m
+++ matlab/burst-analysis/makeSequence.m
@@ -1,34 +1,35 @@
-%MAKESEQUENCE output t, x, y sequences for every spike in target event
-%Read time and x,y location for every spike in target event (preBurst or 
-%nonBurst)from spikeTime as features, return an array. 
+%MAKESEQUENCE return sequence for every spike in target event
+%retrieve t,x,y from spikeTime info for every spike in target event 
+%as features, return an 1D array 
 %  
-%   Syntax:  sequence = makeSequence(spikeTime, n_spikes)
+%   Syntax:  sequence = makeSequence(h5file, spikeTime, n_spikes)
 %   
 %   Input:
+%   h5file    - BrainGrid simulation result (.h5)
 %   spikeTime - matrix contains spikeTime info for target event
 %   n_spikes  - number of spikes in target event
 %
-%   Output:
+%   Return:
 %   sequence  - array of size 1 x (3*n_spikes)
 
-%Author: Jewel Y. Lee (jewel87@uw.edu)
-function sequence = makeSequence(spikeTime, n_spikes)
-load('config.mat','xloc')
-load('config.mat','yloc')
+%Author: Jewel Y. Lee (jewel87@uw.edu) 4/1/2018 last updated.
+function sequence = makeSequence(h5file, spikeTime, window)
+xloc = uint16((hdf5read([h5file '.h5'], 'xloc'))'); xloc=xloc+1;    % x location
+yloc = uint16((hdf5read([h5file '.h5'], 'yloc'))'); yloc=yloc+1;    % y location
 t_offset = spikeTime(1,1);          % relative time offset
-features = n_spikes*3;              % 3 features (t,x,y) for each spike
-counter = 1;
-sequence = zeros(1,features);
-for row = 1:size(spikeTime,1)       % for each timestep in this event
+features = 3*window;                % 3 features (t,x,y) for each spike
+seq = zeros(3,window);
+for row = size(spikeTime,1):-1:1    % for each timestep in this event
     t = spikeTime(row,1) - t_offset;     
     col = 2;                        % neuron idx starts at 2nd column
     % for all spikes in this timestep
-    while spikeTime(row,col) > 0 && counter < features  
-        sequence(counter) = t;
-        sequence(counter+1) = xloc(spikeTime(row,col));
-        sequence(counter+2) = yloc(spikeTime(row,col));
+    while spikeTime(row,col) > 0 && window > 0  
+        seq(1,window) = t;
+        seq(2,window) = xloc(spikeTime(row,col));
+        seq(3,window) = yloc(spikeTime(row,col));
         col = col + 1;
-        counter = counter + 3;
+        window = window - 1;
     end   
 end
+sequence = reshape(seq,[1,features]);
 end
---
+++ matlab/burst-analysis/precursorSeq.m
@@ -0,0 +1,15 @@
+% script to generate precursor sequences with different window and gap size
+clc;
+h5file = 'tR_1.0--fE_0.90_10000';
+window = [70 50 30 20 10];
+gapPre = [0 10 20 30 50];
+gapNon = 1000;                  % fixed
+% output files
+for i = 1:length(window)
+    for j = 1:length(gapPre)
+        [prefile, nonfile] = getPrecursors(window(i), gapPre(j), gapNon);
+        makeAllSequence(h5file, pre, window(i));
+    end
+    makeAllSequence(h5file, non, window(i));   
+end
+
